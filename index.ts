0e1200b62b29095dba55e2c6f56ce897ee919cc2
Edison Tsang <s115239@ap.be>
2024-03-01 02:24:18 +0000

    Git add all

---
+++ index.ts
@@ -0,0 +1,33 @@
+import gamesData from './games.json';
+import { Developer, Game } from './interfaces';
+import * as readline from 'readline-sync';
+
+
+const games: Game[] = gamesData;
+let again:boolean = true;
+
+while(again){
+    console.log('Welcome to the JSON data viewer!');
+    console.log('1. View all data');
+    console.log('2. Filter by ID');
+    console.log('3. Exit');
+    
+    let choice:number = readline.questionInt("");
+    
+    if(choice === 1){
+        games.forEach((game) => {
+            console.log(`Game: ${game.name}  (${game.id})`);
+            console.log('\n-----------------\n');
+          });
+    } else if (choice === 2){
+        let idFilterChoice:string = readline.question("Please enter the ID you want to filter by: ");
+        const filteredGame = games.find((game) => game.id === idFilterChoice);
+    }
+    else{
+    again = false;
+    }
+}
+
+
+
+
---
+++ interfaces.ts
@@ -0,0 +1,20 @@
+export interface Developer {
+    id: string;
+    name: string;
+    isActive: boolean;
+    gamesPublished: number;
+  }
+  
+  export interface Game {
+    id: string;
+    name: string;
+    description: string;
+    age: number;
+    multiplayer: boolean;
+    releaseDate: string;
+    imageUrl: string;
+    genre: string;
+    platforms: string[];
+    developers: Developer;
+  }
+  
