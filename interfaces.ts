2249482d57462d185a68e3d5f0e4be287fdfce4c
Edison Tsang <s115239@ap.be>
2024-03-01 02:58:12 +0000

    jsons veranderingen

--- interfaces.ts
+++ interfaces.ts
@@ -1,25 +1,21 @@
-/*export interface Developer {
+  export interface Developer {
     id: string;
     name: string;
     isActive: boolean;
     gamesPublished: number;
-  }*/
+  }
   
   export interface Game {
     id: string;
     name: string;
     description: string;
-    age: number;
+   age: number;
     multiplayer: boolean;
     releaseDate: string;
-    imageUrl: string;
-    genre: string;
-    platforms: string[];
-    developers: {
-      id: string;
-      Name: string;
-      IsActive: boolean;
-      gamesPublished: number;
-    };
-  }
+   imageUrl: string;
+   genre: string;
+   platforms: string[];
+    developers: Developer;
+    }
+  
   
